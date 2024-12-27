81f8600f79056498664567308af482ee4c761198
goropikari <goropikari56@gmail.com>
2024-12-28 06:01:10 +0900

    remove zenn command existance check

--- lua/telescope_zenn/init.lua
+++ lua/telescope_zenn/init.lua
@@ -4,31 +4,31 @@ local pickers = require('telescope.pickers')
 local conf = require('telescope.config').values
 local zenn_finder = require('telescope_zenn.finder')
 
-local ensure_zenn_cli_executable = function()
-  -- Ensure cwd is node.js project root
-  local file = Path:new('./package.json')
-  print(vim.inspect(file))
-  if not file:exists() then
-    vim.notify('./package.json not found. Please ensure that you are at the zenn directory root.', vim.log.levels.WARN)
-    return false
-  end
-
-  -- Ensure zenn-cli is installed
-  local res, code = Job:new({
-    command = 'npx',
-    args = { 'zenn', '--help' },
-  }):sync()
-  if code ~= 0 then
-    vim.notify('Zenn cli is not executable. Please ensure that you are at the zenn directory root.', vim.log.levels.WARN)
-    return false
-  end
-  return true
-end
+-- local ensure_zenn_cli_executable = function()
+--   -- Ensure cwd is node.js project root
+--   local file = Path:new('./package.json')
+--   -- print(vim.inspect(file))
+--   if not file:exists() then
+--     vim.notify('./package.json not found. Please ensure that you are at the zenn directory root.', vim.log.levels.WARN)
+--     return false
+--   end
+--
+--   -- Ensure zenn-cli is installed
+--   local res, code = Job:new({
+--     command = 'npx',
+--     args = { 'zenn', '--help' },
+--   }):sync()
+--   if code ~= 0 then
+--     vim.notify('Zenn cli is not executable. Please ensure that you are at the zenn directory root.', vim.log.levels.WARN)
+--     return false
+--   end
+--   return true
+-- end
 
 local article_picker = function(opts)
-  if not ensure_zenn_cli_executable() then
-    return
-  end
+  -- if not ensure_zenn_cli_executable() then
+  --   return
+  -- end
   opts = opts or {}
   pickers
     .new(opts, {
